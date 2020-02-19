959b773dc546e4f3deef4449d6398a00aa738657
Tim van Leeuwen <timvanleeuwen06@gmail.com>
2020-02-19 14:28:09 +0100

    added design and funcionality examplesheet

---
+++ src/data.js
@@ -0,0 +1,27 @@
+const Data = [
+  {
+    id: 123,
+    userName: "Daan",
+    saldo: 0,
+    key: 1,
+    entry: "eten 4 maart",
+    prijs: 10,
+    datum: "04 03 2020"
+  },
+  {
+    id: 123,
+    userName: "Daan",
+    saldo: 0,
+    key: 2,
+    entry: "eten 3 april",
+    prijs: 25,
+    datum: "03 04 2020"
+  },
+
+  {
+    userName: "Tim",
+    amount: 0
+  }
+];
+
+export default Data;
