e7e83bb93f28f7d944ec656c8219950099421288
DR-84 <58080204+DR-84@users.noreply.github.com>
2020-03-10 13:58:59 +0100

    nothing commited

--- src/Container.js
+++ src/Container.js
@@ -1,31 +1,31 @@
-import React, { Component } from "react";
-import Data from "./Data";
-import BalanceOverview from "./BalanceOverview";
-import ExpensesOverview from "./ExpensesOverview";
-import Input from "./Input";
+import React, { Component } from 'react';
+import Data from './Data';
+import BalanceOverview from './BalanceOverview';
+import ExpensesOverview from './ExpensesOverview';
+import Input from './Input';
 
 class Container extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: Data
-    };
-    this.sort = this.sort.bind(this);
-  }
+	constructor() {
+		super();
+		this.state = {
+			data: Data
+		};
+		this.sort = this.sort.bind(this);
+	}
 
-  sort() {
-    console.log("sortbutton clicked");
-  }
+	sort() {
+		console.log('sortbutton clicked');
+	}
 
-  render() {
-    return (
-      <div>
-        <BalanceOverview data={this.state.data} />
-        <Input />
-        <ExpensesOverview data={this.state.data} sort={this.sort} />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<BalanceOverview data={this.state.data} />
+				<Input />
+				<ExpensesOverview data={this.state.data} sort={this.sort} />
+			</div>
+		);
+	}
 }
 
 export default Container;
