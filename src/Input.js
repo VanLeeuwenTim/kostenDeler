b80f0fe8d40af1094e56bfc89f3d40010632687e
DR-84 <58080204+DR-84@users.noreply.github.com>
2020-03-10 13:57:43 +0100

    added form

--- src/Input.js
+++ src/Input.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React from 'react';
 
-const Input = () => {
-  return <h2>Input</h2>;
+const Input = props => {
+	return (
+		<form onSubmit={props.onSubmitHandler}>
+			<input name="paidBy" type="text" placeholder="--Paid by--" />
+			<input name="amount" type="text" placeholder="--Amount--" />
+			<select name="currency">
+				<option value="EUR">EUR</option>
+				<option value="CAD">CAD</option>
+				<option value="USD">USD</option>
+				<option value="GBP">GBP</option>
+			</select>
+			<input name="date" type="text" placeholder="date" />
+			<input name="discription" type="text" placeholder="--Event--" />
+			<input name="addedBy" type="text" placeholder="--Paid by--" />
+			<input type="submit" value="Submit" onClick={props.makeId} />
+		</form>
+	);
 };
 
 export default Input;
