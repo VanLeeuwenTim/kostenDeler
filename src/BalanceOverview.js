5ebd54adbcb86a1edd40e14c831efe55144b68b6
Tim van Leeuwen <timvanleeuwen06@gmail.com>
2020-03-11 16:27:34 +0100

    added balancetotal functionality

--- src/BalanceOverview.js
+++ src/BalanceOverview.js
@@ -2,12 +2,30 @@ import React from "react";
 
 const BalanceOverview = props => {
   const userNames = props.data.map(item => item.userName);
+  console.log("userNames", userNames);
   const singleName = userNames.filter((name, index, names) => {
     return names.indexOf(name) === index;
   });
+  console.log("singleName", singleName);
 
-  const listNames = singleName.map(element => {
-    return <li>{element}</li>;
+  const listNames = singleName.map(item => {
+    console.log("item:", item);
+    const name = item;
+
+    const filterMethod = props.data.map(element =>
+      element.userName === name ? element.amount : 0
+    );
+    console.log("name:", name);
+    console.log("filterMethod", filterMethod);
+    const amountTotal = filterMethod.reduce((a, b) => a + b, 0);
+    console.log("amountTotalPP", name, amountTotal);
+
+    return (
+      <li>
+        {name}
+        {amountTotal}
+      </li>
+    );
   });
 
   return (
--- src/Input.js
+++ src/Input.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const Input = props => {
-	return (
-		<form onSubmit={props.onSubmitHandler}>
-			<input name="paidBy" type="text" placeholder="--Paid by--" />
-			<input name="amount" type="text" placeholder="--Amount--" />
-			<select name="currency">
-				<option value="EUR">EUR</option>
-				<option value="CAD">CAD</option>
-				<option value="USD">USD</option>
-				<option value="GBP">GBP</option>
-			</select>
-			<input name="date" type="text" placeholder="--date--" />
-			<input name="description" type="text" placeholder="--Event--" />
-			<input name="addedBy" type="text" placeholder="--Added by--" />
-			<input type="submit" value="Submit" onClick={props.makeId} />
-		</form>
-	);
+  return (
+    <form onSubmit={props.onSubmitHandler}>
+      <input name="paidBy" type="text" placeholder="--Paid by--" />
+      <input name="amount" type="number" placeholder="--Amount--" />
+      <select name="currency">
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="USD">USD</option>
+        <option value="GBP">GBP</option>
+      </select>
+      <input name="date" type="text" placeholder="--date--" />
+      <input name="description" type="text" placeholder="--Event--" />
+      <input name="addedBy" type="text" placeholder="--Added by--" />
+      <input type="submit" value="Submit" onClick={props.makeId} />
+    </form>
+  );
 };
 
 export default Input;
