9ccd7f5ff6c4b054cf9ded687ad3230eecbc2aec
Tim van Leeuwen <timvanleeuwen06@gmail.com>
2020-03-12 17:00:51 +0100

    added styling

--- src/BalanceOverview.js
+++ src/BalanceOverview.js
@@ -1,49 +1,49 @@
-import React from 'react';
+import React from "react";
 //import BalanceItems from './BalanceItems';
 
 const BalanceOverview = props => {
-	const userNames = props.data.map(item => item.userName);
-	const singleName = userNames.filter((name, index, names) => {
-		return names.indexOf(name) === index;
-	});
-	const userAmount = singleName.length;
-	const allAmounts = props.data.map(item => item.amount);
-	const totalAllAmounts = allAmounts.reduce((a, b) => a + b, 0);
-	const averageAmount = totalAllAmounts / userAmount;
+  const userNames = props.data.map(item => item.userName);
+  const singleName = userNames.filter((name, index, names) => {
+    return names.indexOf(name) === index;
+  });
+  const userAmount = singleName.length;
+  const allAmounts = props.data.map(item => item.amount);
+  const totalAllAmounts = allAmounts.reduce((a, b) => a + b, 0);
+  const averageAmount = totalAllAmounts / userAmount;
 
-	const singleUserData = singleName.map(item => {
-		const name = item;
-		const filterMethod = props.data.map(element =>
-			element.userName === name ? element.amount : 0
-		);
-		const amountTotal = filterMethod.reduce((a, b) => a + b, 0);
-		const balancePP = Math.round((amountTotal - averageAmount) * 100) / 100;
+  const singleUserData = singleName.map(item => {
+    const name = item;
+    const filterMethod = props.data.map(element =>
+      element.userName === name ? element.amount : 0
+    );
+    const amountTotal = filterMethod.reduce((a, b) => a + b, 0);
+    const balancePP = Math.round((amountTotal - averageAmount) * 100) / 100;
 
-		return (
-			<tr>
-				<td>{name}</td>
-				<td>{balancePP}</td>
-				<td>{amountTotal}</td>
-			</tr>
-		);
-	});
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{balancePP}</td>
+        <td>{amountTotal}</td>
+      </tr>
+    );
+  });
 
-	return (
-		<div>
-			<h2>BalanceOverview:</h2>
-			<table style={{ width: '100%' }}>
-				<thead>
-					<tr className="table-header">
-						<th className="table-row__item">Username:</th>
-						<th className="table-row__item">Balance:</th>
+  return (
+    <div id="balance-overview">
+      <h2>BalanceOverview:</h2>
+      <table style={{ width: "100%" }}>
+        <thead>
+          <tr className="table-header">
+            <th className="table-row__item">Username:</th>
+            <th className="table-row__item">Balance:</th>
 
-						<th className="table-row__item">Total amount paid:</th>
-					</tr>
-				</thead>
-				<tbody>{singleUserData}</tbody>
-			</table>
-		</div>
-	);
+            <th className="table-row__item">Total amount paid:</th>
+          </tr>
+        </thead>
+        <tbody>{singleUserData}</tbody>
+      </table>
+    </div>
+  );
 };
 
 export default BalanceOverview;
--- src/Container.js
+++ src/Container.js
@@ -1,80 +1,80 @@
-import React, { Component } from 'react';
-import Data from './Database';
-import BalanceOverview from './BalanceOverview';
-import ExpensesOverview from './ExpensesOverview';
-import Input from './Input';
+import React, { Component } from "react";
+import Data from "./Database";
+import BalanceOverview from "./BalanceOverview";
+import ExpensesOverview from "./ExpensesOverview";
+import Input from "./Input";
 
 class Container extends Component {
-	constructor() {
-		super();
-		this.state = {
-			data: Data
-		};
-		this.sort = this.sort.bind(this);
-		this.deleteItem = this.deleteItem.bind(this);
-		this.onSubmitHandler = this.onSubmitHandler.bind(this);
-	}
+  constructor() {
+    super();
+    this.state = {
+      data: Data
+    };
+    this.sort = this.sort.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.onSubmitHandler = this.onSubmitHandler.bind(this);
+  }
 
-	onSubmitHandler(event) {
-		event.preventDefault();
-		const uniqueId = Date.now();
-		const input = event.target;
-		const targetPaidBy = input.paidBy.value;
-		const targetAmount = parseInt(input.amount.value);
-		const targetCurrency = input.currency.value;
-		const targetDate = input.date.value;
-		const targetDescription = input.description.value;
-		const targetAddedBy = input.addedBy.value;
-		const targetKey = uniqueId;
-		this.setState(prevState => {
-			const updatedData = prevState.data;
-			updatedData.push({
-				key: targetKey,
-				userName: targetPaidBy,
-				amount: targetAmount,
-				currency: targetCurrency,
-				date: targetDate,
-				description: targetDescription,
-				addedBy: targetAddedBy
-			});
-			return {
-				data: updatedData
-			};
-		});
-	}
-	sort() {
-		console.log('sortbutton clicked');
-	}
+  onSubmitHandler(event) {
+    event.preventDefault();
+    const uniqueId = Date.now();
+    const input = event.target;
+    const targetPaidBy = input.paidBy.value;
+    const targetAmount = parseInt(input.amount.value);
+    const targetCurrency = input.currency.value;
+    const targetDate = input.date.value;
+    const targetDescription = input.description.value;
+    const targetAddedBy = input.addedBy.value;
+    const targetKey = uniqueId;
+    this.setState(prevState => {
+      const updatedData = prevState.data;
+      updatedData.push({
+        key: targetKey,
+        userName: targetPaidBy,
+        amount: targetAmount,
+        currency: targetCurrency,
+        date: targetDate,
+        description: targetDescription,
+        addedBy: targetAddedBy
+      });
+      return {
+        data: updatedData
+      };
+    });
+  }
+  sort() {
+    console.log("sortbutton clicked");
+  }
 
-	deleteItem(event) {
-		const deleteItemId = event.target.id;
-		console.log(event.target);
-		this.setState(prevState => {
-			const updatedItemList = prevState.data;
-			updatedItemList.splice(
-				updatedItemList.findIndex(item => item.key == deleteItemId),
-				1
-			);
-			return {
-				data: updatedItemList
-			};
-		});
-	}
+  deleteItem(event) {
+    const deleteItemId = event.target.id;
+    console.log(event.target);
+    this.setState(prevState => {
+      const updatedItemList = prevState.data;
+      updatedItemList.splice(
+        updatedItemList.findIndex(item => item.key == deleteItemId),
+        1
+      );
+      return {
+        data: updatedItemList
+      };
+    });
+  }
 
-	render() {
-		console.log('dataBase', this.state.data);
-		return (
-			<div>
-				<BalanceOverview data={this.state.data} />
-				<Input onSubmitHandler={this.onSubmitHandler} />
-				<ExpensesOverview
-					data={this.state.data}
-					sort={this.sort}
-					deleteItem={this.deleteItem}
-				/>
-			</div>
-		);
-	}
+  render() {
+    console.log("dataBase", this.state.data);
+    return (
+      <div>
+        <Input onSubmitHandler={this.onSubmitHandler} />
+        <BalanceOverview data={this.state.data} />
+        <ExpensesOverview
+          data={this.state.data}
+          sort={this.sort}
+          deleteItem={this.deleteItem}
+        />
+      </div>
+    );
+  }
 }
 
 export default Container;
