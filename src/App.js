387487c786af63d330a9ca40c6bf6fa96efa648f
Tim van Leeuwen <timvanleeuwen06@gmail.com>
2020-03-19 17:08:22 +0100

    changed some text

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ export default function App() {
         <nav className="navbar navbar-expand-lg navbar-light fixed-top">
           <div className="container">
             <Link className="navbar-brand" to={"/sign-in"}>
-              positronX.io
+              expensesOverview Daan and Tim
             </Link>
             <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
               <ul className="navbar-nav ml-auto">
